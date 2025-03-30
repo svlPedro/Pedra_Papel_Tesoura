aea9cb81c670bda16c87841ad8f40b0173aeb0cd
Pedro Soares <pedrosoaresvl.tech@gmail.com>
2025-03-30 20:00:00 -0300

    adição de métodos na classe Jogada

--- src/main/java/entidades/Jogada.java
+++ src/main/java/entidades/Jogada.java
@@ -1,6 +1,7 @@
 package entidades;
 
 import java.util.Date;
+import java.util.Random;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -18,9 +19,36 @@ public class Jogada {
 	private String jogador1;
 	@Column(nullable = false)
 	private String jogador2;
+	private String jogada1;
+	private String jogada2;
 	private String resultado;
 	@Temporal(TemporalType.DATE)
 	private Date data;
+	
+	public void verificarResultado() {
+		if (jogada1.equals(jogada2)) {
+	        resultado = "Empate";
+	    } else if (jogada1.equals("Pedra") && jogada2.equals("Tesoura")) {
+	        resultado = jogador1 + " Venceu";
+	    } else if (jogada1.equals("Tesoura") && jogada2.equals("Papel")) {
+	        resultado = jogador1 + " Venceu";
+	    } else if (jogada1.equals("Papel") && jogada2.equals("Pedra")) {
+	        resultado = jogador1 + " Venceu";
+	    } else if (jogada2.equals("Pedra") && jogada1.equals("Tesoura")) {
+	        resultado = jogador2 + " Venceu";
+	    } else if (jogada2.equals("Tesoura") && jogada1.equals("Papel")) {
+	        resultado = jogador2 + " Venceu";
+	    } else if (jogada2.equals("Papel") && jogada1.equals("Pedra")) {
+	        resultado = jogador2 + " Venceu";
+	    }
+	}
+	
+	public void atribuirJogadas() {
+		 String[] opcoes = {"Pedra", "Papel", "Tesoura"};
+	     Random rd = new Random();
+	     jogada1 = opcoes[rd.nextInt(opcoes.length)]; 
+	     jogada2 = opcoes[rd.nextInt(opcoes.length)]; 
+	}
 	
 	public Integer getId() {
 		return id;
@@ -51,5 +79,21 @@ public class Jogada {
 	}
 	public void setData(Date data) {
 		this.data = data;
+	}
+
+	public String getJogada1() {
+		return jogada1;
+	}
+
+	public void setJogada1(String jogada1) {
+		this.jogada1 = jogada1;
+	}
+
+	public String getJogada2() {
+		return jogada2;
+	}
+
+	public void setJogada2(String jogada2) {
+		this.jogada2 = jogada2;
 	}	
 }
